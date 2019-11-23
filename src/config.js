a67a0c7665622562523fead39d6c5a58a4cdd72e
Eric Wang <ericwang27759@gmail.com>
2019-11-22 20:10:03 -0500

    deploying

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ module.exports = {
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/recipro',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/recipro-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-    JWT_EXPIRY: process.env.JWT_EXPIRY || '600s',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '18000s',
 };
